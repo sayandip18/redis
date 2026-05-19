fb598002bd29d94f8b58e4d438cbe79533aeec0a
Sayandip Halder <sayandiph4@gmail.com>
2026-05-20 00:55:03 +0530

    command pipeline

--- core/resp.go
+++ core/resp.go
@@ -5,30 +5,6 @@ import (
 	"fmt"
 )
 
-func readSimpleString(data []byte) (string, int, error) {
-	pos := 1
-
-	for ; data[pos] != '\r'; pos++ {
-	}
-
-	return string(data[1:pos]), pos + 2, nil
-}
-
-func readError(data []byte) (string, int, error) {
-	return readSimpleString(data)
-}
-
-func readInt64(data []byte) (int64, int, error) {
-	pos := 1
-	var value int64 = 0
-
-	for ; data[pos] != '\r'; pos++ {
-		value = value*10 + int64(data[pos]-'0')
-	}
-
-	return value, pos + 2, nil
-}
-
 func readLength(data []byte) (int, int) {
 	pos, length := 0, 0
 	for pos = range data {
@@ -41,6 +17,55 @@ func readLength(data []byte) (int, int) {
 	return 0, 0
 }
 
+// reads a RESP encoded simple string from data and returns
+// the string, the delta, and the error
+func readSimpleString(data []byte) (string, int, error) {
+	// first character +
+	pos := 1
+
+	for ; data[pos] != '\r'; pos++ {
+	}
+
+	return string(data[1:pos]), pos + 2, nil
+}
+
+// reads a RESP encoded error from data and returns
+// the error string, the delta, and the error
+func readError(data []byte) (string, int, error) {
+	return readSimpleString(data)
+}
+
+// reads a RESP encoded integer from data and returns
+// the intger value, the delta, and the error
+func readInt64(data []byte) (int64, int, error) {
+	// first character :
+	pos := 1
+	var value int64 = 0
+
+	for ; data[pos] != '\r'; pos++ {
+		value = value*10 + int64(data[pos]-'0')
+	}
+
+	return value, pos + 2, nil
+}
+
+// reads a RESP encoded string from data and returns
+// the string, the delta, and the error
+func readBulkString(data []byte) (string, int, error) {
+	// first character $
+	pos := 1
+
+	// reading the length and forwarding the pos by
+	// the lenth of the integer + the first special character
+	len, delta := readLength(data[pos:])
+	pos += delta
+
+	// reading `len` bytes as string
+	return string(data[pos:(pos + len)]), pos + len + 2, nil
+}
+
+// reads a RESP encoded array from data and returns
+// the array, the delta, and the error
 func readArray(data []byte) (interface{}, int, error) {
 	// first character *
 	pos := 1
@@ -61,25 +86,10 @@ func readArray(data []byte) (interface{}, int, error) {
 	return elems, pos, nil
 }
 
-
-func readBulkString(data []byte) (string, int, error) {
-	// first character $
-	pos := 1
-
-	// reading the length and forwarding the pos by
-	// the lenth of the integer + the first special character
-	len, delta := readLength(data[pos:])
-	pos += delta
-
-	// reading `len` bytes as string
-	return string(data[pos:(pos + len)]), pos + len + 2, nil
-}
-
 func DecodeOne(data []byte) (interface{}, int, error) {
 	if len(data) == 0 {
 		return nil, 0, errors.New("no data")
 	}
-
 	switch data[0] {
 	case '+':
 		return readSimpleString(data)
@@ -95,39 +105,35 @@ func DecodeOne(data []byte) (interface{}, int, error) {
 	return nil, 0, nil
 }
 
-func Decode(data []byte) (interface{}, error) {
+func Decode(data []byte) ([]interface{}, error) {
 	if len(data) == 0 {
 		return nil, errors.New("no data")
 	}
-	value, _, err := DecodeOne(data)
-	return value, err
-}
-
-func DecodeArrayString(data []byte) ([]string, error) {
-	value, err := Decode(data)
-	if err != nil {
-		return nil, err
+	var values []interface{} = make([]interface{}, 0)
+	var index int = 0
+	for index < len(data) {
+		value, delta, err := DecodeOne(data[index:])
+		if err != nil {
+			return values, err
+		}
+		index = index + delta
+		values = append(values, value)
 	}
-
-	ts := value.([]interface{})
-	tokens := make([]string, len(ts))
-	for i := range tokens {
-		tokens[i] = ts[i].(string)
-	}
-
-	return tokens, nil
+	return values, nil
 }
 
 func Encode(value interface{}, isSimple bool) []byte {
 	switch v := value.(type) {
-		case string:
-			if isSimple {
-				return []byte(fmt.Sprintf("+%s\r\n", v))
-			}
-			return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
-		case int, int8, int16, int32, int64:
-			return []byte(fmt.Sprintf(":%d\r\n", v))
-		default:
-			return RESP_NIL
+	case string:
+		if isSimple {
+			return []byte(fmt.Sprintf("+%s\r\n", v))
+		}
+		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
+	case int, int8, int16, int32, int64:
+		return []byte(fmt.Sprintf(":%d\r\n", v))
+	case error:
+		return []byte(fmt.Sprintf("-%s\r\n", v))
+	default:
+		return RESP_NIL
 	}
 }
--- server/sync_tcp.go
+++ server/sync_tcp.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net"
@@ -12,7 +11,15 @@ import (
 	"github.com/sayandip/redis/core"
 )
 
-func readCommand(c io.ReadWriter) (*core.RedisCmd, error) {
+func toArrayString(ai []interface{}) ([]string, error) {
+	as := make([]string, len(ai))
+	for i := range ai {
+		as[i] = ai[i].(string)
+	}
+	return as, nil
+}
+
+func readCommands(c io.ReadWriter) (core.RedisCmds, error) {
 	// TODO: Max read in one shot is 512 bytes
 	// To allow input > 512 bytes, then repeated read until
 	// we get EOF or designated delimiter
@@ -21,27 +28,28 @@ func readCommand(c io.ReadWriter) (*core.RedisCmd, error) {
 	if err != nil {
 		return nil, err
 	}
-	
-	tokens, err := core.DecodeArrayString(buf[:n])
+
+	values, err := core.Decode(buf[:n])
 	if err != nil {
 		return nil, err
 	}
 
-	return &core.RedisCmd{
-		Cmd:  strings.ToUpper(tokens[0]),
-		Args: tokens[1:],
-	}, nil
-}
-
-func respondError(err error, c io.ReadWriter) {
-	c.Write([]byte(fmt.Sprintf("-%s\r\n", err)))
-}
-
-func respond(cmd *core.RedisCmd, c io.ReadWriter) {
-	err := core.EvalAndRespond(cmd, c)
-	if err != nil {
-		respondError(err, c)
+	var cmds []*core.RedisCmd = make([]*core.RedisCmd, 0)
+	for _, value := range values {
+		tokens, err := toArrayString(value.([]interface{}))
+		if err != nil {
+			return nil, err
+		}
+		cmds = append(cmds, &core.RedisCmd{
+			Cmd:  strings.ToUpper(tokens[0]),
+			Args: tokens[1:],
+		})
 	}
+	return cmds, nil
+}
+
+func respond(cmds core.RedisCmds, c io.ReadWriter) {
+	core.EvalAndRespond(cmds, c)
 }
 
 func RunSyncTCPServer() {
@@ -49,31 +57,34 @@ func RunSyncTCPServer() {
 
 	var con_clients int = 0
 
-	listener, err := net.Listen("tcp", config.Host+":"+strconv.Itoa(config.Port))
+	// listening to the configured host:port
+	lsnr, err := net.Listen("tcp", config.Host+":"+strconv.Itoa(config.Port))
 	if err != nil {
 		log.Println("err", err)
 		return
 	}
 
 	for {
-		c, err := listener.Accept()
+		// blocking call: waiting for the new client to connect
+		c, err := lsnr.Accept()
 		if err != nil {
 			log.Println("err", err)
 		}
-		con_clients++
+
+		// increment the number of concurrent clients
+		con_clients += 1
 
 		for {
-			cmd, err := readCommand(c)
+			// over the socket, continuously read the command and print it out
+			cmds, err := readCommands(c)
 			if err != nil {
 				c.Close()
-				con_clients--
+				con_clients -= 1
 				if err == io.EOF {
 					break
 				}
 			}
-			respond(cmd, c)
+			respond(cmds, c)
 		}
-
 	}
-
 }
